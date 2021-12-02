9524f66e3a9f24b2caf6069812ccdefdc58c9c26
mtamayo323 <86122824+mtamayo323@users.noreply.github.com>
2021-12-01 17:34:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function countDown(num) {
     }
   }, 1000)
 }
+
+function randomGame(){
+  let num;
+  let times = 0;
+  let timer = setInterval(function(){
+    num = Math.random();
+    times++
+    if(num > .75) {
+      clearInterval(timer);
+      console.log("It took " + times + " try/tries.");
+    }
+  },1000)
+}
